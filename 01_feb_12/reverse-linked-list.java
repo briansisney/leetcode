59374d267888a41d6e15dc9325e49363020fc299
briansisney <brian.sisney@gmail.com>
2021-02-08 22:58:44 -0800

    reverse a linked list

--- 01_feb_12/reverse-linked-list.java
+++ 01_feb_12/reverse-linked-list.java
@@ -10,7 +10,19 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode temp = null;
 
+        if (head == null) {
+            return null;
+        }
+
+        while (head != null) {
+            temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
     }
 }
-
