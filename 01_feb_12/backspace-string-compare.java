a878838639204b5e82b841b67a2934a86c9f1879
briansisney <brian.sisney@gmail.com>
2021-02-12 17:15:05 -0800

    added in problems from today

---
+++ 01_feb_12/backspace-string-compare.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        StringBuilder sbS = new StringBuilder();
+        StringBuilder sbT = new StringBuilder();
+        for(int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            if (ch !='#') {
+                sbS.append(ch);
+
+            } else {
+                if(sbS.length() > 0) {
+                    sbS.deleteCharAt(sbS.length() - 1);
+                }
+
+            }
+        }
+
+        for(int i=0; i<T.length(); i++) {
+            char ch = T.charAt(i);
+            if (ch != '#') {
+                sbT.append(ch);
+            } else {
+                if(sbT.length() > 0) {
+                    sbT.deleteCharAt(sbT.length() - 1);
+                }
+            }
+        }
+
+        return sbS.toString().equals(sbT.toString());
+    }
+}
